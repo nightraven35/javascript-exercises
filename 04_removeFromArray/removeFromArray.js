b194b922b2ee3a63a72dea9c1a00a4a36bfb3f1e
Aldair Pedro <greatestattackturtle@gmail.com>
2022-09-14 16:51:54 -0400

    Complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
 const removeFromArray = function() {
-
+    if (arguments[0]) {
+        let current_Argument = "";
+        let second_Counter = 0;
+        for (let i = 0; i < arguments.length - 1; i++){
+            current_Argument = arguments[i + 1];
+            second_Counter = arguments[0].length
+            for(let j = 0; j < second_Counter; j++){
+                if (arguments[0][j] === current_Argument){
+                    arguments[0].splice(j, 1);
+                }
+            }
+        }
+    }
+    return arguments[0];
 };
 
 // Do not edit below this line
