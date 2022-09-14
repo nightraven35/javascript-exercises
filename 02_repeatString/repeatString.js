470505be988e1d0f97ddc78ce24c82efc19701cf
Aldair Pedro <greatestattackturtle@gmail.com>
2022-09-13 20:54:34 -0400

    Finish exercise 2 with all but one test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let result = "";
+    let i = 0;
+    while(i < num){
+        result += string;
+        i++ 
+    }
+    return result;
 };
 
 // Do not edit below this line
