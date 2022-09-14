531c7545e5613fb2174161274f61610f9a5452dc
Aldair Pedro <greatestattackturtle@gmail.com>
2022-09-14 17:13:57 -0400

    complete exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let result = false;
+    if (year % 4 == 0){
+        result = true;
+        if (year % 100 == 0){
+            if (year % 400 != 0){
+                result = false;
+            } 
+        }
+    }
+    return result;
 };
 
 // Do not edit below this line
