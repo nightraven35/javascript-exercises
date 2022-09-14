e36e84cd4d5834cfb55f1cd7846286bae815ac92
Aldair Pedro <greatestattackturtle@gmail.com>
2022-09-14 16:18:03 -0400

    Complete exersice 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reversedString = "";
+    if(string){
+        let list = string.split("");
+        for (let i = 0; i < string.length; i++){
+            reversedString += list.pop();
+        }
+        return reversedString;
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
