359e367f8934c98d03bb8dc5008b453403f7b92a
Aldair Pedro <greatestattackturtle@gmail.com>
2022-09-14 17:02:48 -0400

    Complete exercise 5 except for error test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(first_Integer, second_Integer) {
+    let result = 0;
+    if (first_Integer < second_Integer){
+        for (let i = first_Integer; i <= second_Integer; i++){
+            result += i;
+       }
+    }
+    for (let i = second_Integer; i <= first_Integer; i++){
+        result += i;
+    }
+    return result;
 };
 
 // Do not edit below this line
